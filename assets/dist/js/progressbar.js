492bf01ec2ea5a4b656aff937be9629a4e395609
chency0315 <s1083714@mail.yzu.edu.tw>
2024-03-01 15:26:54 +0800

    added images from the website

---
+++ assets/dist/js/progressbar.js
@@ -0,0 +1,7 @@
+function updateProgressBar(){
+   const{scrollTop, scrollHeight} = document.documentElement; 
+   const scrollPercent = `${(scrollTop / (scrollHeight-window.innerHeight))*100}%`;
+   console.log(scrollPercent);
+   document.querySelector("#progress-bar").style.setProperty('--progress',scrollPercent);
+}
+document.addEventListener('scroll',updateProgressBar);
